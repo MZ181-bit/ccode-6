0cddfd2a579cee9e9e7bceaaeeffe2881446ba8e
MZ181-bit <maazjuha181@gmail.com>
2025-11-03 20:07:12 +0530

    Add Fibonacci sequence implementation in ccode.c
    Implement Fibonacci sequence printing.

---
+++ ccode.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int a = 0, b = 1, c, i;
+  printf("%d %d ", a, b);
+  for (i = 2; i < 10; i++) {
+    c = a + b;
+    printf("%d ", c);
+    a = b;
+    b = c;
+  }
+  return 0;
+}
